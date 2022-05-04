c5901b258f10f9c121eea59afcdb5b8147e1a158
DESKTOP-UUVO7JG\sato <aimkitsou.014@gmail.com>
2022-05-04 16:28:08 +0900

    middleware トレイリングスラッシュのリダイレクト処理 tsファイルに変更

--- middleware/slash-redirect.ts
+++ middleware/slash-redirect.ts
@@ -1,4 +1,4 @@
-export default function({ route, redirect }) {
+export default function ({ route, redirect }: {route: any, redirect: any}) {
   if (route.path.slice(-1) !== '/') {
     redirect(301, route.path + '/')
   }
