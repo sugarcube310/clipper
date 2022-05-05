4d956a231c1ef757bc48a176072d153048855f49
DESKTOP-UUVO7JG\sato <aimkitsou.014@gmail.com>
2022-05-05 12:32:17 +0900

    不要コード削除

--- store/mutations.js
+++ store/mutations.js
@@ -1,6 +1,6 @@
 export default {
   /**** ユーザー情報の取得 ****/
-  getUserData (state, payload, name) {
+  getUserData (state, payload) {
     state.user.uid = payload.uid
     state.user.email = payload.email
     state.user.name = payload.name ? payload.name : ''
