e80d822c900d2e3ae4250b5a74f5fc3c27521c3c
DESKTOP-UUVO7JG\sato <aimkitsou.014@gmail.com>
2022-05-10 21:00:00 +0900

    アカウント設定ページのコードを整理

--- pages/mypage.vue
+++ pages/mypage.vue
@@ -2,166 +2,161 @@
   <div class="page-container -mypage">
     <PageBackButton />
 
-    <v-row class="user__profile">
-      <v-col cols="12">
-        <div
-          class="mx-auto mb-3 user__icon"
-          :class="{ '-edit': isEditProfileMode }"
-        >
+    <div class="user__profile">
+      <v-row>
+        <v-col cols="12">
           <div
-            v-if="user.image"
-            class="user__icon-image"
+            class="mx-auto mb-3 user__icon"
+            :class="{ '-edit': isEditProfileMode }"
           >
-            <figure>
-              <img :src="profile.image ? profile.image : user.image" alt="">
-            </figure>
-          </div>
-          <div
-            v-else
-            class="user__icon-image -no-setting"
-          >
-            <span class="mdi mdi-account-outline"></span>
-          </div>
-          <label
-            for="input_file"
-            class="inputButton"
-          >
-            画像を変更
-            <input
-              id="input_file"
-              style="display: none;"
-              type="file"
-              accept="img/*"
-              @change="inputFile"
+            <div
+              v-if="user.image"
+              class="user__icon-image"
             >
-          </label>
-        </div>
-      </v-col>
-      <v-col
-        cols="12"
-        class="d-flex flex-column align-center justify-center user__name"
-      >
-        <p
-          v-if="!isEditProfileMode"
-          class="mb-0 text-center"
+              <figure>
+                <img :src="profile.image ? profile.image : user.image" alt="">
+              </figure>
+            </div>
+            <div
+              v-else
+              class="user__icon-image -no-setting"
+            >
+              <span class="mdi mdi-account-outline"></span>
+            </div>
+            <label
+              for="input_file"
+              class="inputButton"
+            >
+              画像を変更
+              <input
+                id="input_file"
+                style="display: none;"
+                type="file"
+                accept="img/*"
+                @change="inputFile"
+              >
+            </label>
+          </div>
+        </v-col>
+      </v-row>
+
+      <v-row v-if="!isEditProfileMode">
+        <v-col
+          cols="12"
+          class="d-flex flex-column align-center justify-center user__name"
         >
-          {{ user.name }}
-        </p>
-        <v-form
-          v-else
-          class="form -mypage"
-          @submit.prevent="updateUser()"
+          <p class="mb-0 text-center">
+            {{ user.name }}
+          </p>
+        </v-col>
+        <v-col cols="12">
+          <p class="mb-0 text-center user__releases">
+            公開クリップ数：{{ user.releases }}件
+          </p>
+        </v-col>
+        <v-col
+          cols="12"
+          class="d-flex flex-column align-center justify-center user__introduction"
         >
-          <v-text-field
-            v-model="profile.name"
-            label="ユーザー名"
-            hide-details
-            outlined
-            color="secondary"
-            class="rounded-lg mb-8"
-          ></v-text-field>
-        </v-form>
-      </v-col>
-      <v-col
-        v-if="!isEditProfileMode"
-        cols="12"
-        class="mt-1"
-      >
-        <p class="mb-0 text-center user__releases">
-          公開クリップ数：{{ user.releases }}件
-        </p>
-      </v-col>
-      <v-col
-        cols="12"
-        class="d-flex flex-column align-center justify-center user__introduction"
-      >
-        <p
-          v-if="!isEditProfileMode && user.introduction"
-          class="mb-0 text-center"
+          <p
+            v-if="user.introduction"
+            class="mb-0 text-center"
+          >
+            {{ user.introduction }}
+          </p>
+        </v-col>
+        <v-col
+          cols="12"
+          class="text-center mt-3"
         >
-          {{ user.introduction }}
-        </p>
-        <v-form
-          v-else-if="isEditProfileMode"
-          class="form -mypage"
-          @submit.prevent="updateUser()"
-        >
-          <v-textarea
-            v-model="profile.introduction"
-            label="自己紹介文"
-            hide-details
-            outlined
-            color="secondary"
+          <v-btn
+            color="accent"
+            depressed
             class="rounded-lg"
-          ></v-textarea>
-        </v-form>
-      </v-col>
-    </v-row>
-    <v-row v-if="!isEditProfileMode">
-      <v-col
-        cols="12"
-        class="text-center mt-3"
-      >
-        <v-btn
-          color="accent"
-          depressed
-          class="rounded-lg"
-          height="44"
-          width="180"
-          :loading="isLoading"
-          :disabled="isLoading"
-          @click="switchEditMode()"
+            height="44"
+            width="180"
+            :loading="isLoading"
+            :disabled="isLoading"
+            @click="switchEditMode()"
+          >
+            プロフィールを編集
+          </v-btn>
+        </v-col>
+      </v-row>
+
+      <v-row v-else>
+        <v-col
+          cols="12"
+          class="d-flex flex-column align-center justify-center user__name"
         >
-          プロフィールを編集
-        </v-btn>
-      </v-col>
-    </v-row>
-    <v-row v-else>
-      <v-col
-        cols="12"
-        class="text-center mt-2 mb-5"
-      >
-        <v-btn
-          color="accent"
-          depressed
-          class="rounded-lg mr-2"
-          height="44"
-          width="130"
-          :loading="isLoading"
-          :disabled="isLoading"
-          @click="updateUser()"
+          <v-form
+            class="form -mypage"
+            @submit.prevent="updateUser()"
+          >
+            <v-text-field
+              v-model="profile.name"
+              label="ユーザー名"
+              hide-details
+              outlined
+              color="secondary"
+              class="rounded-lg mb-8"
+            ></v-text-field>
+            <v-textarea
+              v-model="profile.introduction"
+              label="自己紹介文"
+              hide-details
+              outlined
+              color="secondary"
+              class="rounded-lg"
+            ></v-textarea>
+          </v-form>
+        </v-col>
+        <v-col
+          cols="12"
+          class="text-center mt-2 mb-5"
         >
-          保存
-        </v-btn>
-        <v-btn
-          color="#e8e8e8"
-          depressed
-          class="rounded-lg ml-2 color-gray__button"
-          height="44"
-          width="130"
-          @click="switchEditMode()"
+          <v-btn
+            color="accent"
+            depressed
+            class="rounded-lg mr-2"
+            height="44"
+            width="130"
+            :loading="isLoading"
+            :disabled="isLoading"
+            @click="updateUser()"
+          >
+            保存
+          </v-btn>
+          <v-btn
+            color="#e8e8e8"
+            depressed
+            class="rounded-lg ml-2 color-gray__button"
+            height="44"
+            width="130"
+            @click="switchEditMode()"
+          >
+            キャンセル
+          </v-btn>
+        </v-col>
+        <v-col
+          cols="12"
+          class="text-center"
         >
-          キャンセル
-        </v-btn>
-      </v-col>
-      <v-col
-        cols="12"
-        class="text-center"
-      >
-        <v-btn
-          color="error"
-          depressed
-          small
-          text
-          class="rounded-lg"
-          height="40"
-          width="140"
-          @click="openDeleteAccountDialog()"
-        >
-          アカウントを削除
-        </v-btn>
-      </v-col>
-    </v-row>
+          <v-btn
+            color="error"
+            depressed
+            small
+            text
+            class="rounded-lg"
+            height="40"
+            width="140"
+            @click="openDeleteAccountDialog()"
+          >
+            アカウントを削除
+          </v-btn>
+        </v-col>
+      </v-row>
+    </div>
 
     <DeleteAccount ref="deleteAccountDialogRef" />
   </div>
@@ -286,7 +281,7 @@ export default defineComponent({
 <style lang="postcss">
 .user__profile {
   margin: 40px auto 0;
-  max-width: 480px;
+  max-width: 440px;
   width: 50%;
 
   @media (--sp) {
@@ -402,15 +397,24 @@ export default defineComponent({
 
   & .user__releases {
     color: var(--color-text-light);
-    letter-spacing: .02em;
+    font-size: 15px;
+    letter-spacing: .04em;
+
+    @media (--sp) {
+      font-size: 14px;
+    }
   }
 
   & .user__introduction {
     color: var(--color-text-light);
     letter-spacing: .04em;
     line-height: 2;
-    margin-top: -28px;
+    margin-top: -32px;
     white-space: pre-line;
+
+    @media (--sp) {
+      font-size: 15px;
+    }
 
     & .v-input {
       width: 100%;
